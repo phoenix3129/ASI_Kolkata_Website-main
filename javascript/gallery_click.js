4b41c019689fab3aa9b2d0b30047247e097770e8
Phoenix <iam.aritraparui@gmail.com>
2024-07-13 12:01:25 +0530

    New

---
+++ javascript/gallery_click.js
@@ -0,0 +1,4 @@
+document.getElementById('gal').addEventListener('click', () => {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.style.display = (dropdown.style.display === 'block') ? 'none' : 'block';
+});
